4b7a35c3b75e244bc76debca9ee54e262ebdb46d
omar <omar.jimenez@mo2o.com>
2019-02-26 10:18:58 +0100

    feat: add devTools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { SonComponent } from './cont/son/son.component';
 import { GrandsonComponent } from './cont/grandson/grandson.component';
 import {StoreModule} from '@ngrx/store';
 import {counterReducer} from './cont/counter.reducer';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,11 @@ import {counterReducer} from './cont/counter.reducer';
   ],
   imports: [
     BrowserModule,
-      StoreModule.forRoot({count: counterReducer})
+      StoreModule.forRoot({count: counterReducer}),
+      StoreDevtoolsModule.instrument({
+        maxAge: 25, // Retains last 25 states
+        logOnly: environment.production, // Restrict extension to log-only mode
+      }),
   ],
   providers: [],
   bootstrap: [AppComponent]
